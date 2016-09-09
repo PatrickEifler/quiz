751e8aa01bf625433f4660be27922177799c9b5c
Patrick <patrickeifler@gmail.com>
2016-09-09 14:17:12 +0200

    Add quiz reducer to the root reducer

--- app/src/reducers/index.js
+++ app/src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import { routerReducer as routing } from 'react-router-redux';
 import quizzes from './quizzes';
+import quiz from './quiz';
 
 const rootReducer = combineReducers({
   routing,
-  quizzes
+  quizzes,
+  quiz
 });
 
 export default rootReducer;
