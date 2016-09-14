ffa31bb56b3b23b878ac6d4b87cea41ec98cd3a7
Patrick <patrickeifler@gmail.com>
2016-09-14 16:50:52 +0200

    Implement shuffle module

---
+++ app/src/lib/shuffle.js
@@ -0,0 +1,21 @@
+'use strict';
+
+export default function shuffle (arr) {
+	var copy = arr.slice();
+	var len = copy.length;
+	var idx;
+	var curr;
+
+	while(len) {
+		
+		//pick one remaining element
+		idx = Math.floor(Math.random() * len--);
+
+		//swap it with the current element
+		curr = copy[len];
+		copy[len] = copy[idx];
+		copy[idx] = curr;
+	}
+
+	return copy;
+}
---
+++ app/test/lib/shuffle_test.js
@@ -0,0 +1,25 @@
+import { assert } from 'chai';
+import shuffle from '../../src/lib/shuffle';
+
+describe('Shuffle Test', () => {
+	it('shuffles a given array and returns a copy of it', () => {
+		const inputArr = [
+			{
+				obj1: 0
+			},
+			{
+				obj2: 0
+			},
+			{
+				obj3: 0
+			},
+			{
+				obj4: 0
+			},
+			{
+				obj5: 0
+			}
+		]
+		assert.notDeepEqual(shuffle(inputArr)[0], inputArr[0]);
+	})
+})
