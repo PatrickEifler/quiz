a4ab60e4f03eb3f52ec35c6800ffb983903e556a
Patrick <patrickeifler@gmail.com>
2016-09-21 21:01:48 +0200

    Cleanup diff

--- app/src/lib/diff.js
+++ app/src/lib/diff.js
@@ -1,13 +1,13 @@
 export default function diff (a, b, predicate) {
-	const diffA = a.filter(el => b.filter(elemB => {
+	const onlyInA = a.filter(el => b.filter(elemB => {
 	    	return predicate ? predicate(elemB, el) : elemB.uid === el.uid;
 	    }).length === 0
 	);
 
-	const diffB = b.filter(el => a.filter(elemA => {
+	const onlyInB = b.filter(el => a.filter(elemA => {
 	    	return predicate ? predicate(elemA, el) : elemA.uid === el.uid;
 	 	}).length === 0
 	);
 
-	return diffA.concat(diffB);
+	return onlyInA.concat(onlyInB);
 }
