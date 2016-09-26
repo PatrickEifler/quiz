23e734fa884fd62e6c9d2deba8d7eeb32375ba76
Patrick <patrickeifler@gmail.com>
2016-09-26 12:27:51 +0200

    Render answer options

--- app/src/components/answer/index.js
+++ app/src/components/answer/index.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import uidGen from '../../lib/uidGen';
+import Input from './input';
+
+const renderAnswerOptions = (answer, callback) => {
+
+	const mapOptions = (options) => options.map(option =>
+		<button 
+			key={uidGen()}
+			onClick={() => callback.call(answer, option)}> 
+			{option.label} 
+		</button>	
+	);
+
+	if (answer && answer.type === 'option') {
+		return mapOptions(answer.options);
+	} else {
+		return ( <Input callback={callback} /> );
+	}
+}
 
 export default (props) => {
 	const { answer, callback, isAsking } = props;
-	console.log('answer:', answer)
-	console.log('answerQuestion Callback:', callback)
-	
+
 	return (
 		<div className={'answer', isAsking ? 'show': 'hide'}>
-			implement answer option here
+			<div className='answer-inner'>
+				{ renderAnswerOptions(answer, callback) }
+			</div>
 		</div>	
 	);
 }
