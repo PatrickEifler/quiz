b31634d02a7a1f1ed25b03e1d77271dd727f1603
Patrick <patrickeifler@gmail.com>
2016-09-28 13:37:51 +0200

    Cleanup store reducer test

--- app/test/reducers/score_test.js
+++ app/test/reducers/score_test.js
@@ -7,62 +7,64 @@ describe('Score Reducer', () => {
 			amount: 0
 		})
 	})
-	it('should add a point to the score amount', () => {
-		assert.deepEqual(scoreReducer({
-			amount: 0
-		}, {
-			type: 'ANSWER_QUESTION',
-			answer: {
-				correct: true,
-				label: 'some answer'
-			},
-			active: {
+	describe('Answer Question', () => {
+		it('should add a point to the score amount', () => {
+			assert.deepEqual(scoreReducer({
+				amount: 0
+			}, {
+				type: 'ANSWER_QUESTION',
 				answer: {
-					options: [{
-						label: 'this is correct'
-					}]	
+					correct: true,
+					label: 'some answer'
+				},
+				active: {
+					answer: {
+						options: [{
+							label: 'this is correct'
+						}]	
+					}
 				}
-			}
-		}), {
-			amount: 1
-		})
-	});
-	it('should remove a point from the score amount', () => {
-		assert.deepEqual(scoreReducer({
-			amount: 2
-		}, {
-			type: 'ANSWER_QUESTION',
-			answer: {
-				label: 'some answer'
-			},
-			active: {
+			}), {
+				amount: 1
+			})
+		});
+		it('should remove a point from the score amount', () => {
+			assert.deepEqual(scoreReducer({
+				amount: 2
+			}, {
+				type: 'ANSWER_QUESTION',
 				answer: {
-					options: [{
-						label: 'this is correct'
-					}]	
+					label: 'some answer'
+				},
+				active: {
+					answer: {
+						options: [{
+							label: 'this is correct'
+						}]	
+					}
 				}
-			}
-		}), {
-			amount: 1
-		})
-	});
-	it('should not remove a point if score amount is already zero', () => {
-		assert.deepEqual(scoreReducer({
-			amount: 0
-		}, {
-			type: 'ANSWER_QUESTION',
-			answer: {
-				label: 'some answer'
-			},
-			active: {
+			}), {
+				amount: 1
+			})
+		});
+		it('should not remove a point if score amount is already zero', () => {
+			assert.deepEqual(scoreReducer({
+				amount: 0
+			}, {
+				type: 'ANSWER_QUESTION',
 				answer: {
-					options: [{
-						label: 'this is correct'
-					}]	
+					label: 'some answer'
+				},
+				active: {
+					answer: {
+						options: [{
+							label: 'this is correct'
+						}]	
+					}
 				}
-			}
-		}), {
-			amount: 0
-		})
+			}), {
+				amount: 0
+			})
+		});
 	});
 });
