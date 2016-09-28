07ad5dc4dc9f6205906e43403feffe4cc86e6642
Patrick <patrickeifler@gmail.com>
2016-09-28 13:35:06 +0200

    Remove obsolete action

--- app/src/actions/questions/index.js
+++ app/src/actions/questions/index.js
@@ -1,20 +1,10 @@
 import * as types from './constants';
 
-export function setQuestions() {
-	//initially get the questions from the quiz item object
-	//set them on the questions object
-	return (dispatch, getState) => dispatch({
-		type: types.SET_QUESTIONS,
-		allQuestions: getState().quiz.item.questions
-	});
-}
-
 export function askQuestion() {
-	//get the question items from the questions object
 	return (dispatch, getState) => dispatch({
 		type: types.ASK_QUESTION,
 		questions: getState().questions.items
-	})
+	});
 }
 
 export function answerQuestion(answer) {
@@ -22,7 +12,7 @@ export function answerQuestion(answer) {
 		type: types.ANSWER_QUESTION,
 		active: getState().questions.active,
 		answer
-	})
+	});
 }
 
 export function abortQuestion(uid) {
