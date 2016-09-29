9fcd9047a1c23d3fab8b69aa31850ad282393ced
Patrick <patrickeifler@gmail.com>
2016-09-29 10:14:22 +0200

    Add Result route

--- app/src/Routes.js
+++ app/src/Routes.js
@@ -4,14 +4,12 @@ import { routerActions } from 'react-router-redux';
 import App from './containers/App';
 import Quizzes from './containers/Quizzes';
 import Quiz from './containers/Quiz';
+import Result from './containers/Result';
 
 export default (
   <Route path="/" component={App}>
-  	<Route path="/quizzes" component={Quizzes}/>
+  	<Route path="/quizzes" component={Quizzes} />
   	<Route path="/quizzes/:uid" component={Quiz} />
+  	<Route path="/quizzes/:uid/result" component={Result} />
   </Route>
 );
-
-// <Route path="/quizzes/:uid" component={Quiz}>
-// 	<Route path="question" component={Question} />
-//</Route>
