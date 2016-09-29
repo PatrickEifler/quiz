025ad651feff24c1b7c43c8d6eca13166d143ca6
Patrick <patrickeifler@gmail.com>
2016-09-29 11:21:05 +0200

    Add link to result screen

--- app/src/components/result/index.js
+++ app/src/components/result/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router';
 import Title from '../quiz/title';
 import Score from '../score';
 
@@ -8,6 +9,7 @@ export default (props) => {
 		<div className={`quiz-${props.uid} quiz-container quiz-result`}>
 			<Title title={`Result: ${props.title}`} />
 			<Score score={props.score} />
+			<Link to='/quizzes'>Back to quizzes</Link>
 		</div>
 	);
 };
