53fef902328523f4342fe49480bcf2286bef208d
Patrick <patrickeifler@gmail.com>
2016-09-29 10:25:12 +0200

    Add todo

--- app/src/components/result/index.js
+++ app/src/components/result/index.js
@@ -3,9 +3,10 @@ import Title from '../quiz/title';
 import Score from '../score';
 
 export default (props) => {
+	//TODO: Add button which links to Quizzes Overview
 	return (
 		<div className={`quiz-${props.uid} quiz-container quiz-result`}>
-			<Title title={props.title} />
+			<Title title={`Result: ${props.title}`} />
 			<Score score={props.score} />
 		</div>
 	);
